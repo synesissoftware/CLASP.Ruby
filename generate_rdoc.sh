cb345a676cb4412b7d5242294381664ece281140
Matt <stlsoft@gmail.com>
2016-06-11 20:46:47 +1000

    ~ tidying

--- generate_rdoc.sh
+++ generate_rdoc.sh
@@ -11,5 +11,13 @@
 #############################################################################
 
 rm -rfd doc
-rdoc -x build_gem.sh -x run_all_unit_tests.sh -x test -x lib/clasp/old_module.rb -x examples
+rdoc \
+	-x build_gem.sh \
+	-x generate_rdoc.sh \
+	-x run_all_unit_tests.sh \
+	-x test \
+	-x lib/clasp/old_module.rb \
+	-x examples \
+	-x CLASP.gemspec \
+
 
