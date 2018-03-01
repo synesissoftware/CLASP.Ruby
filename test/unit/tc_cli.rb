4833c4abe81ff2154bac54af1be85cd3a14207f7
Matt <stlsoft@gmail.com>
2018-03-01 19:53:30 +1100

    ~ fixed defect in CLASP.show_usage() and CLASP.show_version() parameter checking, which failed to permit new CLASP::Alias type in aliases array

--- test/unit/tc_cli.rb
+++ test/unit/tc_cli.rb
@@ -14,9 +14,9 @@ class Test_CLI < Test::Unit::TestCase
 
 	def test_invalid_aliases_types
 
-		assert_raise_with_message(::TypeError, 'each element in aliases must be a Flag or an Option') { CLASP.show_usage([ 'abc', :def ]) }
+		assert_raise_with_message(::TypeError, 'each element in aliases array must be one of the types CLASP::Flag, CLASP::Option, or CLASP::Alias') { CLASP.show_usage([ 'abc', :def ]) }
 
-		assert_raise_with_message(::TypeError, 'each element in aliases must be a Flag or an Option') { CLASP.show_version([ 'abc', :def ]) }
+		assert_raise_with_message(::TypeError, 'each element in aliases array must be one of the types CLASP::Flag, CLASP::Option, or CLASP::Alias') { CLASP.show_version([ 'abc', :def ]) }
 	end
 end
 
