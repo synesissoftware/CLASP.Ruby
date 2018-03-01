b908acce3b595767166798e24775ad523df4c2bb
Matt <stlsoft@gmail.com>
2018-03-01 19:43:06 +1100

    + added unit-test file for CLI

---
+++ test/unit/tc_cli.rb
@@ -0,0 +1,22 @@
+#! /usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(__FILE__), '../..', 'lib')
+
+require 'clasp'
+
+require 'test/unit'
+
+require 'xqsr3/extensions/test/unit'
+
+require 'stringio'
+
+class Test_CLI < Test::Unit::TestCase
+
+	def test_invalid_aliases_types
+
+		assert_raise_with_message(::TypeError, 'each element in aliases must be a Flag or an Option') { CLASP.show_usage([ 'abc', :def ]) }
+
+		assert_raise_with_message(::TypeError, 'each element in aliases must be a Flag or an Option') { CLASP.show_version([ 'abc', :def ]) }
+	end
+end
+
